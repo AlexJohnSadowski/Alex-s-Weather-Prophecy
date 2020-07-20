3a4f5da5d15069c7e571e4e29eb3a3fd39decd21
Alex <sadovsky.aleksander@gmail.com>
2020-07-20 18:13:19 +0200

    editing content of the summary.js

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -3,9 +3,9 @@ import React from "react";
 const Summary = ({ mean, mode }) => {
   return (
     <div className="summary">
-      <div className="summary__info">Mode value for the upcoming 24 hours - {mode}°c</div>
+      <div className="summary__info">Mode value for the upcoming 24 hours: {mode}°c</div>
       <div className="summary__info">
-        Average temperature for the upcoming 5 days - {Math.round(mean)}°c
+        Average temperature for the upcoming 5 days: {Math.round(mean)}°c
       </div>
     </div>
   );
