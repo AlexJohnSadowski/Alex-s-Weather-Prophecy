98b55b293b983ebfe3fb11d76fef2cd4ff364f18
Alex <sadovsky.aleksander@gmail.com>
2020-07-20 20:32:26 +0200

    import added in tests

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,41 +1,11 @@
+import { reduceArray, ModeValue } from './functions'
 
-/* Tests */
 
 test("should reduce array and count the mean for the upcoming 5 days", () => {
-  expected = 2;
-  expect(reduceArray([2, 2, 2, 2, 2])).toEqual(expected);
+  expect(reduceArray([2, 2, 2, 2, 2])).toEqual(2);
 });
 
 
 test("should find the mode value", () => {
-  expected = 3;
-  expect(ModeValue([1, 3, 3, 3, 3, 3, 4])).toEqual(expected);
+  expect(ModeValue([1, 3, 3, 3, 3, 3, 4])).toEqual(3);
 });
-
-
-/* Functions */
-
-function reduceArray(array) {
-  return array.reduce(function (a, b) {
-    return a + b / 5;
-  }, 0);
-}
-
-
-function ModeValue(list) {
-  let mostFrequent = null,
-    mostFrequentItem;
-  list.sort();
-
-  for (let i = 0; i < list.length; i++) {
-    let single = list[i];
-    let total = list.lastIndexOf(single) - list.indexOf(single) + 1;
-
-    if (total > mostFrequent) {
-      mostFrequentItem = list[i];
-      mostFrequent = total;
-      i = list.lastIndexOf(single) + 1;
-    }
-  }
-  return mostFrequentItem;
-}
